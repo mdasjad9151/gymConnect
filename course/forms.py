fa6efa65598b1de3c41185f0625226ef4502e3ae
mdasjad9151 <asjad.ajic@gmail.com>
2025-05-12 16:44:40 +0530

    add:QR verification

--- course/forms.py
+++ course/forms.py
@@ -6,12 +6,12 @@ class VideoCourseForm(forms.ModelForm):
         model = VideoCourse
         fields = ['title', 'description', 'category', 'fitness_level', 'price', 'thumbnail']
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'w-full p-2 border rounded-xl'}),
-            'description': forms.Textarea(attrs={'class': 'w-full p-2 border rounded-xl', 'rows': 4}),
-            'category': forms.Select(attrs={'class': 'w-full p-2 border rounded-xl'}),
-            'fitness_level': forms.Select(attrs={'class': 'w-full p-2 border rounded-xl'}),
-            'price': forms.NumberInput(attrs={'class': 'w-full p-2 border rounded-xl'}),
-            'thumbnail': forms.ClearableFileInput(attrs={'class': 'w-full p-2 border rounded-xl'}),
+            'title': forms.TextInput(attrs={'class': 'w-full p-2 border bg-white/10 placeholder-black text-black backdrop-blur-lg rounded-xl'}),
+            'description': forms.Textarea(attrs={'class': 'w-full p-2 border bg-white/10 placeholder-black text-black backdrop-blur-lg rounded-xl', 'rows': 4}),
+            'category': forms.Select(attrs={'class': 'w-full p-2 border bg-white/10 placeholder-black text-black backdrop-blur-lg rounded-xl'}),
+            'fitness_level': forms.Select(attrs={'class': 'w-full p-2 border bg-white/10 placeholder-black text-black backdrop-blur-lg rounded-xl'}),
+            'price': forms.NumberInput(attrs={'class': 'w-full p-2 border bg-white/10 placeholder-black text-black backdrop-blur-lg rounded-xl'}),
+            'thumbnail': forms.ClearableFileInput(attrs={'class': 'w-full p-2 border bg-white/10 placeholder-black text-black backdrop-blur-lg rounded-xl'}),
         }
 
 class CourseVideoForm(forms.ModelForm):
@@ -19,9 +19,9 @@ class CourseVideoForm(forms.ModelForm):
         model = CourseVideo
         fields = ['title', 'description', 'video_file', 'duration', 'sequence']
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'w-full p-2 border rounded-xl'}),
-            'description': forms.Textarea(attrs={'class': 'w-full p-2 border rounded-xl', 'rows': 3}),
-            'video_file': forms.ClearableFileInput(attrs={'class': 'w-full p-2 border rounded-xl'}),
-            'duration': forms.TimeInput(attrs={'class': 'w-full p-2 border rounded-xl', 'type': 'time'}),
-            'sequence': forms.NumberInput(attrs={'class': 'w-full p-2 border rounded-xl'}),
+            'title': forms.TextInput(attrs={'class': 'w-full p-2 border bg-white/10 placeholder-black text-black backdrop-blur-lg rounded-xl'}),
+            'description': forms.Textarea(attrs={'class': 'w-full p-2 border bg-white/10 placeholder-black text-black backdrop-blur-lg rounded-xl', 'rows': 3}),
+            'video_file': forms.ClearableFileInput(attrs={'class': 'w-full p-2 border bg-white/10 placeholder-black text-black backdrop-blur-lg rounded-xl'}),
+            'duration': forms.TimeInput(attrs={'class': 'w-full p-2 border bg-white/10 placeholder-black text-black backdrop-blur-lg rounded-xl', 'type': 'time', 'step':'1'}),
+            'sequence': forms.NumberInput(attrs={'class': 'w-full p-2 border bg-white/10 placeholder-black text-black backdrop-blur-lg rounded-xl'}),
         }
